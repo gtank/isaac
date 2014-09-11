2f89e74bc229dbc6530ce094b9ce9f9e67873aa0
George Tankersley <george.tankersley@gmail.com>
2014-09-10 23:09:52 -0700

    update isaac to be a little more idiomatic

--- isaac/isaac.go
+++ isaac/isaac.go
@@ -2,53 +2,59 @@
 ------------------------------------------------------------------------------
 isaac.go: an implementation of Bob Jenkins' random number generator ISAAC based on 'readable.c'
 * 18 Aug 2014 -- direct port of readable.c to Go
+* 10 Sep 2014 -- updated to be more idiomatic Go
 ------------------------------------------------------------------------------
 */
 
 package isaac
 
-/* external results */
-var randrsl [256]uint32
-var randcnt uint32
+type isaac struct {
+	/* external results */
+	randrsl [256]uint32
+	randcnt uint32
 
-/* internal state */
-var mm [256]uint32
-var aa, bb, cc uint32 // zero by default
+	/* internal state */
+	mm         [256]uint32
+	aa, bb, cc uint32
+}
 
-func Isaac() {
+func (r *isaac) isaac() {
 	var x, y uint32
 
-	cc = cc + 1  /* cc just gets incremented once per 256 results */
-	bb = bb + cc /* then combined with bb */
+	r.cc++       /* cc just gets incremented once per 256 results */
+	r.bb += r.cc /* then combined with bb                         */
 
 	for i := 0; i < 256; i++ {
-		x = mm[i]
+		x = r.mm[i]
 
 		switch i % 4 {
 		case 0:
-			aa = aa ^ (aa << 13)
-			break
+			r.aa ^= (r.aa << 13)
 		case 1:
-			aa = aa ^ (aa >> 6)
-			break
+			r.aa ^= (r.aa >> 6)
 		case 2:
-			aa = aa ^ (aa << 2)
-			break
+			r.aa ^= (r.aa << 2)
 		case 3:
-			aa = aa ^ (aa >> 16)
-			break
+			r.aa ^= (r.aa >> 16)
 		}
 
-		aa = mm[(i+128)%256] + aa
-		y = mm[(x>>2)%256] + aa + bb
-		bb = mm[(y>>10)%256] + x
+		r.aa = r.mm[(i+128)%256] + r.aa
+		y = r.mm[(x>>2)%256] + r.aa + r.bb
+		r.bb = r.mm[(y>>10)%256] + x
 
-		mm[i] = y
-		randrsl[i] = bb
+		r.mm[i] = y
+		r.randrsl[i] = r.bb
+
+		/* Note that bits 2..9 are chosen from x but 10..17 are chosen
+		   from y.  The only important thing here is that 2..9 and 10..17
+		   don't overlap.  2..9 and 10..17 were then chosen for speed in
+		   the optimized version (rand.c) */
+		/* See http://burtleburtle.net/bob/rand/isaac.html
+		   for further explanations and analysis. */
+
 	}
 }
 
-/* if (flag!=0), then use the contents of randrsl[] to initialize mm[]. */
 func mix(a, b, c, d, e, f, g, h uint32) (uint32, uint32, uint32, uint32, uint32, uint32, uint32, uint32) {
 	a ^= b << 11
 	d += a
@@ -77,10 +83,9 @@ func mix(a, b, c, d, e, f, g, h uint32) (uint32, uint32, uint32, uint32, uint32,
 	return a, b, c, d, e, f, g, h
 }
 
-func Randinit(flag bool) {
+/* if (flag==true), then use the contents of randrsl[] to initialize mm[]. */
+func (r *isaac) randInit(flag bool) {
 	var a, b, c, d, e, f, g, h uint32
-	aa, bb, cc = 0, 0, 0
-
 	a, b, c, d, e, f, g, h = 0x9e3779b9, 0x9e3779b9, 0x9e3779b9, 0x9e3779b9, 0x9e3779b9, 0x9e3779b9, 0x9e3779b9, 0x9e3779b9
 
 	for i := 0; i < 4; i++ {
@@ -89,56 +94,72 @@ func Randinit(flag bool) {
 
 	for i := 0; i < 256; i += 8 { /* fill mm[] with messy stuff */
 		if flag { /* use all the information in the seed */
-			a += randrsl[i]
-			b += randrsl[i+1]
-			c += randrsl[i+2]
-			d += randrsl[i+3]
-			e += randrsl[i+4]
-			f += randrsl[i+5]
-			g += randrsl[i+6]
-			h += randrsl[i+7]
+			a += r.randrsl[i]
+			b += r.randrsl[i+1]
+			c += r.randrsl[i+2]
+			d += r.randrsl[i+3]
+			e += r.randrsl[i+4]
+			f += r.randrsl[i+5]
+			g += r.randrsl[i+6]
+			h += r.randrsl[i+7]
 		}
 		a, b, c, d, e, f, g, h = mix(a, b, c, d, e, f, g, h)
-		mm[i] = a
-		mm[i+1] = b
-		mm[i+2] = c
-		mm[i+3] = d
-		mm[i+4] = e
-		mm[i+5] = f
-		mm[i+6] = g
-		mm[i+7] = h
+		r.mm[i] = a
+		r.mm[i+1] = b
+		r.mm[i+2] = c
+		r.mm[i+3] = d
+		r.mm[i+4] = e
+		r.mm[i+5] = f
+		r.mm[i+6] = g
+		r.mm[i+7] = h
 	}
 
 	if flag { /* do a second pass to make all of the seed affect all of mm */
 		for i := 0; i < 256; i += 8 {
-			a += mm[i]
-			b += mm[i+1]
-			c += mm[i+2]
-			d += mm[i+3]
-			e += mm[i+4]
-			f += mm[i+5]
-			g += mm[i+6]
-			h += mm[i+7]
+			a += r.mm[i]
+			b += r.mm[i+1]
+			c += r.mm[i+2]
+			d += r.mm[i+3]
+			e += r.mm[i+4]
+			f += r.mm[i+5]
+			g += r.mm[i+6]
+			h += r.mm[i+7]
 			a, b, c, d, e, f, g, h = mix(a, b, c, d, e, f, g, h)
-			mm[i] = a
-			mm[i+1] = b
-			mm[i+2] = c
-			mm[i+3] = d
-			mm[i+4] = e
-			mm[i+5] = f
-			mm[i+6] = g
-			mm[i+7] = h
+			r.mm[i] = a
+			r.mm[i+1] = b
+			r.mm[i+2] = c
+			r.mm[i+3] = d
+			r.mm[i+4] = e
+			r.mm[i+5] = f
+			r.mm[i+6] = g
+			r.mm[i+7] = h
 		}
 	}
 
-	Isaac()
-	randcnt = 256
+	r.isaac()     /* fill in the first set of results */
+	r.randcnt = 0 /* reset the counter */
 }
 
-func Randcnt() uint32 {
-	return randcnt
+/* there is no official method for doing this, but just writing the key to the
+ * state array is how the demo code does it */
+func (r *isaac) Seed(key string) {
+	for idx, c := range key {
+		if idx == len(r.randrsl) {
+			break
+		}
+		r.randrsl[idx] = uint32(c)
+		r.randInit(true)
+	}
 }
 
-func Randrsl() [256]uint32 {
-	return randrsl
+/* retrieve the next number in the sequence */
+func (r *isaac) Rand() uint32 {
+	rnd := r.randrsl[r.randcnt]
+	r.randcnt++
+	if r.randcnt == uint32(len(r.randrsl)) {
+		// reset for another 256
+		r.isaac()
+		r.randcnt = 0
+	}
+	return rnd
 }
